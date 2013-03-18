466f63f53c4c3246ce93c178ae1e7f223b15481f
Andrew Lee <andrew@firebase.com>
2013-03-17 20:03:07 -0700

    Made json data files generate

--- sample_data/gen_data.js
+++ sample_data/gen_data.js
@@ -1,16 +1,94 @@
 var fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function(err, data) {
-  var j = JSON.parse(data);
-  for(var id in j) {
-    j[id].pos = {l: Math.floor(Math.random() * 800), t: Math.floor(Math.random() * 600)};
+var outputDir = "output";
+
+var outputFiles = [
+  {
+    name: "hn1.json",
+    src: ["common.json", "hn.json"]
+  },
+  {
+    name: "hn2.json",
+    src: ["common.json", "hn.json"]
+  },
+  {
+    name: "hn3.json",
+    src: ["common.json", "hn.json"]
+  },
+  {
+    name: "hn4.json",
+    src: ["common.json", "hn.json"]
+  },
+  {
+    name: "programming.json",
+    src: ["common.json", "programming.json"]
+  },
+  {
+    name: "startups.json",
+    src: ["common.json", "startups.json"]
+  },
+  {
+    name: "sf.json",
+    src: ["common.json", "sf.json"]
+  },
+  {
+    name: "nyc.json",
+    src: ["common.json", "nyc.json"]
+  },
+  {
+    name: "firebase.json",
+    src: ["common.json", "firebase.json"]
   }
-  var toWrite = JSON.stringify(j, undefined, 2);
-  fs.writeFile(process.argv[3], toWrite, function(err) {
-    if(err) {
-      console.log("write failed");
-    } else {
-      console.log("write success!");
-    }
-  });
+]
+
+//Make sure the output dir exists.
+fs.mkdir(outputDir, function(e) {
+  start();
 });
+
+function start() {
+  for(var i in outputFiles) {
+    generateFile(outputFiles[i].name, outputFiles[i].src);
+  }
+}
+
+function generateFile(name, sources) {
+  loadAndMergeWords(sources, function () {
+    var outData = [];
+    for (var id in words) {
+      outData.push({
+        text:words[id],
+        l:Math.floor(Math.random() * 800),
+        t:Math.floor(Math.random() * 600)
+      });
+    }
+    var toWrite = JSON.stringify(outData, undefined, 2);
+    fs.writeFile(outputDir + "/" + name, toWrite, function (err) {
+      if (err) {
+        console.log("write failed");
+      } else {
+        console.log("write success");
+      }
+    });
+  });
+}
+
+function loadAndMergeWords(sources, continuation) {
+  var i = 0;
+  var words = [];
+  for(var x = 0; x < sources.length; x++) {
+    console.log("Loading: " + sources[x]);
+    fs.readFile(sources[x], 'utf8', function(err, data) {
+      var j = data.split("\n");
+      for(var q in j) {
+        words.push(j[q]);
+      }
+      i++;
+      if(i == sources.length) {
+        continuation(words);
+      }
+    });
+  }
+}
+
+
